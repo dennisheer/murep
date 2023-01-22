39d92d12b4d9df0380bc12b34e96dd26e59daf27
Dennis Heer <dennisheer@users.noreply.github.com>
2023-01-22 21:40:50 +0100

    Add Carbon Components UI (#7)
    * Add Carbon Components
    
    * Add basic Carbon Component button

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,7 +1,21 @@
+<script lang="ts">
+  import { Button } from 'carbon-components-svelte';
+
+  let counter = 0;
+
+  function incrementCounter(): void {
+    counter += 1;
+  }
+</script>
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
 <h1 class="text-3xl font-bold underline">Hello world!</h1>
+
+<Button on:click={incrementCounter}>Increment</Button>
+
+<span>{counter}</span>
 
 <style lang="postcss">
   :global(html) {
