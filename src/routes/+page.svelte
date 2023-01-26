428135408eed1fe5adf4f72ecf09623b5ca5071f
Fabio Maienschein <fabio.maienschein@gmail.com>
2023-01-26 22:36:07 +0100

    Replace project files to make it work with skeleton

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,24 +1,5 @@
-<script lang="ts">
-  import { Button } from 'carbon-components-svelte';
+<!-- YOU CAN DELETE EVERYTHING IN THIS PAGE -->
 
-  let counter = 0;
-
-  function incrementCounter(): void {
-    counter += 1;
-  }
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
-
-<h1 class="text-3xl font-bold underline">Hello world!</h1>
-
-<Button on:click={incrementCounter}>Increment</Button>
-
-<span>{counter}</span>
-
-<style lang="postcss">
-  :global(html) {
-    background-color: theme(colors.gray.100);
-  }
-</style>
+<div class="container h-full mx-auto flex justify-center items-center">
+	<h1>Let's get cracking bones!</h1>
+</div>
