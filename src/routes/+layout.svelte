151f342e92788746eacd8420e9741e0a33b0cb7d
Dennis Heer <dennisheer@protonmail.ch>
2023-01-28 12:05:33 +0100

    Implement PR feedback

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,30 +1,26 @@
 <script>
   import '@skeletonlabs/skeleton/themes/theme-skeleton.css';
   import '@skeletonlabs/skeleton/styles/all.css';
-  import '../app.postcss';
-  import { AppBar, AppRail, AppRailTile, AppShell, Avatar } from '@skeletonlabs/skeleton';
+  import '../app.css';
+  import { AppBar, AppRail, AppRailTile, AppShell } from '@skeletonlabs/skeleton';
 </script>
 
-<div class="h-full">
-  <AppShell>
-    <svelte:fragment slot="header"
-      ><AppBar>
-        <svelte:fragment slot="lead"><h1>murep</h1></svelte:fragment>
-        <svelte:fragment slot="trail"><Avatar src="https://i.pravatar.cc/" /></svelte:fragment>
-      </AppBar></svelte:fragment
-    >
-    <svelte:fragment slot="sidebarLeft"
-      ><AppRail>
-        <AppRailTile tag="a" href="/">Dashboard</AppRailTile>
-        <AppRailTile tag="a" href="/repertoire">Repertoire</AppRailTile>
-        <AppRailTile tag="a" href="/ensembles">Ensembles</AppRailTile>
-      </AppRail></svelte:fragment
-    >
-    <!-- <svelte:fragment slot="pageHeader">Page Header</svelte:fragment> -->
-    <!-- Router Slot -->
-    <div class="mx-auto max-w-7xl sm:px-6 lg:px-64"><slot /></div>
-    <!-- ---- / ---- -->
-    <!-- <svelte:fragment slot="pageFooter">Page Footer</svelte:fragment>
-  <svelte:fragment slot="footer">Footer</svelte:fragment> -->
-  </AppShell>
-</div>
+<AppShell>
+  <svelte:fragment slot="header">
+    <AppBar>
+      <svelte:fragment slot="lead">
+        <h1>murep</h1>
+      </svelte:fragment>
+    </AppBar>
+  </svelte:fragment>
+  <svelte:fragment slot="sidebarLeft">
+    <AppRail>
+      <AppRailTile tag="a" href="/" class="text-xs md:text-md">Dashboard</AppRailTile>
+      <AppRailTile tag="a" href="/repertoire" class="text-xs md:text-md">Repertoire</AppRailTile>
+      <AppRailTile tag="a" href="/ensembles" class="text-xs md:text-md">Ensembles</AppRailTile>
+    </AppRail>
+  </svelte:fragment>
+  <div class="mx-auto max-w-7xl px-6 py-4 md:px-12 md:py-8 lg:px-24 lg:py-16">
+    <slot />
+  </div>
+</AppShell>
